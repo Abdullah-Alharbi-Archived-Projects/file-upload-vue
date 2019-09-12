2b38a6d11981409d27f7ce586f67a1fdb899e834
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-12 05:15:34 +0300

    added style

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="file-upload">
-    <div class="box">
-      <!-- TODO: add icon -->
-      <div>
-        <h2>Upload File</h2>
+    <label for="file-input">
+      <div class="box">
+        <!-- TODO: add icon -->
+        <div>
+          <h1>Upload File</h1>
+        </div>
+        <div>
+          <h4>Drag and drop or click here</h4>
+        </div>
       </div>
-      <div>
-        <h4>Drag and drop or click here</h4>
-      </div>
-    </div>
+    </label>
   </div>
 </template>
 
@@ -19,4 +21,25 @@ export default {
 </script>
 
 <style scoped>
+.file-upload {
+  margin: 25px;
+}
+
+.box {
+  display: flex;
+  width: 100%;
+  background-color: #1abc9c;
+  border: 5px dashed lightgreen;
+  flex-direction: column;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  padding: 10px;
+  height: 150px;
+}
+
+.box > * {
+  color: #ffffff;
+}
 </style>
