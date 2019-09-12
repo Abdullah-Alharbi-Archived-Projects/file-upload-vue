7a62f823cdb7305234b9d577ba3b9460849f2c60
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-12 05:17:36 +0300

    added enctype attr

--- src/components/FileInput.vue
+++ src/components/FileInput.vue
@@ -1,22 +1,21 @@
 <template>
   <div class="file-input">
-    <form method="POST">
-      <input type="file" :name="name" id="file-input" v-show="false">
+    <form method="POST" enctype="multipart/form-data">
+      <input type="file" :name="name" id="file-input" v-show="false" />
     </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'file-input',
+  name: "file-input",
   data() {
     return {
-      name: 'custom-upload'
-    }
+      name: "custom-upload"
+    };
   }
-}
+};
 </script>
 
 <style>
-
 </style>
