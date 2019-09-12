964741299241f7b5300e91be13295ea1b5609876
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-12 05:18:38 +0300

    import components

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,37 @@
 <template>
   <div id="app">
-    <h1>Hello, World</h1>
+    <div class="w-400">
+      <file-input />
+      <file-upload />
+    </div>
   </div>
 </template>
 
 <script>
+import FileUpload from "./components/FileUpload";
+import FileInput from "./components/FileInput";
+
 export default {
   name: "app",
-  components: {}
+  components: { FileUpload, FileInput }
 };
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
 html,
 body {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+.w-400 {
+  width: 700px;
+  margin: 0 auto;
 }
 </style>
