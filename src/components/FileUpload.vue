bb4f995fb48f8864ea33d801c1e5493b1e4933b6
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-12 04:51:53 +0300

    FileUpload component

---
+++ src/components/FileUpload.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="file-upload">
+    <div class="box">
+      <!-- TODO: add icon -->
+      <div>
+        <h2>Upload File</h2>
+      </div>
+      <div>
+        <h4>Drag and drop or click here</h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "file-upload"
+};
+</script>
+
+<style scoped>
+</style>
