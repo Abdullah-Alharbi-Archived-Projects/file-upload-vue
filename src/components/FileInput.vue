bfffff4b22d62c67a25059419e94561113851a62
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-12 04:48:32 +0300

    FileInput component

---
+++ src/components/FileInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="file-input">
+    <form method="POST">
+      <input type="file" :name="name" id="file-input" v-show="false">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'file-input',
+  data() {
+    return {
+      name: 'custom-upload'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
